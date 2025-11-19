53f573f56d494c1429cfd15b088f344b673c13c9
Colin Leung <colin@cowise.co>
2025-11-20 02:03:44 +0800

    Update test DB options and bump version to 0.0.3
    Changed the Bun SQLite Database options in tests to use 'create: false' instead of 'fileMustExist: true'. Bumped package version to 0.0.3.

--- tests/index.js
+++ tests/index.js
@@ -3,7 +3,7 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url)).replace(/\/$/
 
 import DatabaseAdapter from '../classes/adapter/database/BunSqlite.mjs';
 import { Database } from "bun:sqlite";
-const db = new Database('/Users/colin/Documents/code/frameworks/lionrockjs/adapter/database/bun/tests/db/empty.sqlite', {readwrite: true, fileMustExist: true} )
+const db = new Database('/Users/colin/Documents/code/frameworks/lionrockjs/adapter/database/bun/tests/db/empty.sqlite', {readwrite: true, create: false} )
 
 //const db = await DatabaseAdapter.create(`${__dirname}/db/empty.sqlite`);
 //console.log("Database opened:", db.database);
